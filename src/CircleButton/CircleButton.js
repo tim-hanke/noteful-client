88d9c39a21e1019b403fb44cc4bad17e4dae58cf
Timothy Hanke <tim.hanke@gmail.com>
2020-10-01 06:33:27 -0400

    added PropTypes check to CircleButton component

--- src/CircleButton/CircleButton.js
+++ src/CircleButton/CircleButton.js
@@ -1,19 +1,25 @@
-import React from 'react'
-import './CircleButton.css'
+import React from "react";
+import PropTypes from "prop-types";
+import "./CircleButton.css";
 
 export default function NavCircleButton(props) {
-  const { tag, className, childrenm, ...otherProps } = props
+  const { tag, className, children, ...otherProps } = props;
 
   return React.createElement(
     props.tag,
     {
-      className: ['NavCircleButton', props.className].join(' '),
-      ...otherProps
+      className: ["NavCircleButton", props.className].join(" "),
+      ...otherProps,
     },
     props.children
-  )
+  );
 }
 
-NavCircleButton.defaultProps ={
-  tag: 'a',
-}
+NavCircleButton.defaultProps = {
+  tag: "a",
+};
+
+NavCircleButton.propTypes = {
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]).isRequired,
+  className: PropTypes.string,
+};
