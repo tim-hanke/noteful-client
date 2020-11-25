160b7fcc7ba84f2cf24567c10eb41efdb2ed9d05
Timothy Hanke <tim.hanke@gmail.com>
2020-11-25 00:06:55 -0500

    added endpoint environment variable

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
-  API_ENDPOINT: "https://cryptic-ridge-57013.herokuapp.com/api",
+  API_ENDPOINT:
+    process.env.REACT_APP_API_ENDPOINT ||
+    "https://cryptic-ridge-57013.herokuapp.com/api",
   API_KEY: process.env.REACT_APP_API_KEY,
 };
